48ec2b2631f596185e576b7d83a8ef4141420ce6
AravindSirivelli <aravindsirivelli97@gmail.com>
2022-12-02 13:56:35 +0530

    fixed routes and App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import "./App.css";
-import { MainContainer } from "./components/MainContainer";
-import { SideBar } from "./components/Sidebar/SideBar";
-import { Header } from "./components/Header";
-import Favorite from "./components/Pages/Favorite";
-import History from "./components/Pages/History";
-import Repeated from "./components/Pages/Repeated";
-import Playlist from "./components/Pages/Playlist";
-import { Routes, Route, Outlet } from "react-router-dom";
-import Search from "./components/Pages/Search";
-import { useState } from "react";
-import Player from "./components/Pages/Player";
-import AudioPlayer from "./components/AudioPlayer.component";
+// import { MainContainer } from "./components/MainContainer";
+// import { SideBar } from "./components/Sidebar/SideBar";
+// import { Header } from "./components/Header";
+// import Favorite from "./components/Pages/Favorite";
+// import History from "./components/Pages/History";
+// import Repeated from "./components/Pages/Repeated";
+// import Playlist from "./components/Pages/Playlist";
+// import { Routes, Route, Outlet } from "react-router-dom";
+// import Search from "./components/Pages/Search";
+// import { useState } from "react";
+// import Player from "./components/Pages/Player";
+// import AudioPlayer from "./components/AudioPlayer.component";
+import RouteConfig from "./routes/RouteConfig";
 
 function App() {
-  const [Data, setData] = useState("");
-  function searchResult(result) {
-    setData(result);
-  }
+  // const [Data, setData] = useState("");
+  // function searchResult(result) {
+  //   setData(result);
+  // }
 
   return (
-    <div className="App">
-      <Header searchResult={searchResult} />
-      <div className="flex">
-        <SideBar />
-        <Routes>
-          <Route index path="/" element={<MainContainer />} />
-          <Route exact path="/Favorite" element={<Favorite />} />
-          <Route exact path="/History" element={<History />} />
-          <Route exact path="/Repeated" element={<Repeated />} />
-          <Route exact path="/Playlist" element={<Playlist />} />
-          <Route path="/Search" element={<Search searchData={Data} />} />
-          <Route exact path="/Player" element={<Player />} />
-        </Routes>
-      </div>
-      <AudioPlayer />
+    // <div className="App">
+    //   <Header searchResult={searchResult} />
+    //   <div className="flex">
+    //     <SideBar />
+    //     <Routes>
+    //       <Route index path="/" element={<MainContainer />} />
+    //       <Route exact path="/Favorite" element={<Favorite />} />
+    //       <Route exact path="/History" element={<History />} />
+    //       <Route exact path="/Repeated" element={<Repeated />} />
+    //       <Route exact path="/Playlist" element={<Playlist />} />
+    //       <Route path="/Search" element={<Search searchData={Data} />} />
+    //       <Route exact path="/Player" element={<Player />} />
+    //     </Routes>
+    //   </div>
+    //   <AudioPlayer />
 
-      <div className="background"></div>
-    </div>
+    //   <div className="background"></div>
+    // </div>
+    <RouteConfig/>
   );
 }
 
--- src/routes/RouteConfig.js
+++ src/routes/RouteConfig.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register';
-import Rhythem from '../components/Rhythem/Rhythem.App.componenet';
+// import Rhythem from '../components/Rhythem/Rhythem.App.componenet';
 
 const RouteConfig = () => {
     return (
@@ -10,7 +10,7 @@ const RouteConfig = () => {
             <Route path='/' element={<Login />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/player' element={<Rhythem />} />
+            {/* <Route path='/player' element={<Rhythem />} /> */}
         </Routes>
     )
 }
