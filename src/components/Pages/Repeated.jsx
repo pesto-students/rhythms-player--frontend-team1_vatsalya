71e078c6a0585f3de9eabba35cd367078c8409cc
ANmishra-a <anujm2266@gmail.com>
2022-12-08 01:36:39 +0530

    playlist

--- src/components/Pages/Repeated.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Repeated() {
-  return <div className=" bg-green-400">Repeated</div>;
-}
-
-export default Repeated;
--- src/context/user.component.jsx
+++ src/context/user.component.jsx
@@ -1,18 +1,81 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import { API } from "../config/APIConfig";
 
-// these are the actual value
 export const UserContext = createContext({
-  currentUser: {
-    userName: "",
-    userFav: [],
-    userHistory: [],
-    userPlaylist: [],
-  },
-  setCurrentUser: () => null,
+  currentUser: [], // user id
+  setCurrentUser: () => null, // sets the current user id once he logs in first fetch the user is from master table
+  userPlaylist: [], // these are the name and is of the playlist of the user
+  setPlaylist: () => null, // it runs and set the user playlist name and id
+  userLiked: [], // these are the song id of songs user liked
+  setsUserLiked: () => null, // this gets the data of songs user like
+  addPlaylistToPlayListArray: () => null,
+  getUserPlaylist: () => null,
 });
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+  const [userPlaylist, setPlaylist] = useState([]);
+  const addPlaylistToPlayListArray = (PlaylistName) => {
+    createPlaylist(PlaylistName);
+  }; // ?
+  const [userLiked, setsUserLiked] = useState([]);
+  const getUserPlaylist = () => {
+    var data = JSON.stringify({
+      userId: "3",
+    });
+
+    var config = {
+      method: "get",
+      url: `${API.BACKEND_BASE_URL}/playlists`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        userPlaylist(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+    userPlaylist,
+    setPlaylist,
+    getUserPlaylist,
+    userLiked,
+    setsUserLiked,
+    addPlaylistToPlayListArray,
+  };
+
+  const createPlaylist = (playlistName) => {
+    var data = JSON.stringify({
+      name: playlistName,
+    });
+
+    var config = {
+      method: "post",
+      url: `${API.BACKEND_BASE_URL}/playlists`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
